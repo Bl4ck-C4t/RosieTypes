ecfd9d589b8cc278a82c38c39669432e0c30f59c
Bl4ckC4t <stef4oben88@gmail.com>
2022-11-13 20:44:48 +0200

    Changed to Hash Router

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
 
               <Routes>
                   <Route path="/" element={ <HomePage /> } />
+                  {/*<Route path="/RosieTypes" element={ <HomePage /> } />*/}
                   <Route path="/test" element={ <TestPage /> } />
                   <Route path="/anxious" element={ <AnxiousPage /> } />
                   <Route path="/saviour" element={ <SaviourPage /> } />
